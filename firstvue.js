a9ea29a708bb69f58efcbab3551e8e8fa1bca7b3
jcanofuentes <jcanofuentes@gmail.com>
2022-05-02 11:01:19 +0200

    Life-cycle sample

---
+++ firstvue.js
@@ -0,0 +1,21 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    title_01: 'Project January',
+    title_02: 'Project February',
+    title_03: 'Project March',
+    post_01: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and ",
+    post_02: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and.",
+    post_03: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more"
+    }
+});
+
+const app2 = new Vue({
+  el: '#app2',
+  data: {
+    message: 'Summary',
+    title_01: 'Client 1 - Project February',
+    title_02: 'Client 2 - Project February',
+    title_03: 'Client 1 - Project March',
+    }
+});
